b80ab4e74f3ce6bce6abb836d55d069644f5d96f
kevin <kmunene858@gmail.com>
2022-06-19 17:11:36 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
+
+
+// let list = document.getElementById("list")
+// list.appendChild(div)
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector("form")
+  form.addEventListener("submit", (event) =>{
+    event.preventDefault()
+    addList(event.target["new-task-description"].value)
+    form.reset()
+  })
 });
+
+function addList(input){
+  let li = document.createElement('li')
+	let btn = document.createElement('button')
+	btn.textContent = 'x'
+	btn.addEventListener('click', deleteList)
+	li.textContent = `${input}  `
+	document.querySelector('#list').appendChild(li)
+	li.appendChild(btn)
+  
+}
+
+function deleteList(event){
+	event.target.parentNode.remove()
+  }
